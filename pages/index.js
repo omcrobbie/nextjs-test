57a4882f66380627d208566dbb0df041450e12cb
omcrobbie <omcrobbie@gmail.com>
2018-06-05 16:59:23 -0400

    init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Header from '../components/header';
+import Layout from '../components/MyLayout';
+
+const Index = () => (
+    <Layout>
+        <p>Hello Next.js</p>
+    </Layout>
+)
+export default Index
