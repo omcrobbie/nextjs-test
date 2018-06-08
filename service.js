aeec8b16c69c6eade5dd99ef7bd364999e5ed47f
omcrobbie <omcrobbie@gmail.com>
2018-06-08 09:25:08 -0400

    reorganization

--- service.js
+++
@@ -1,8 +0,0 @@
-import fetch from 'isomorphic-unfetch'
-export default (url) => {
-    return new Promise(async (resolve, reject) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        resolve(data);
-    })
-}
